94ccdbb000c1561b80f6266a70202ab5a3c7b38a
Jake Herrmann <herrmann.jake@protonmail.com>
2024-01-25 14:24:43 -0600

    Created SQLTypeConversion to standardize casting from SQL strings and back

---
+++ ia/src/main/java/rrhs/xc/ia/data/database/SQLTypeConversion.java
@@ -0,0 +1,52 @@
+package rrhs.xc.ia.data.database;
+
+import java.time.LocalDate;
+
+public class SQLTypeConversion {
+    
+    private SQLTypeConversion() {}
+
+    public static String convert(double x) {
+        return "'"+x+"'";
+    }
+
+    public static String convert(int x) {
+        return "'"+x+"'";
+    }
+
+    public static String convert(String x) {
+        return "'"+x+"'";
+    }
+
+    public static String convert(LocalDate x) {
+        return "'" + x.toString() + "'";
+    }
+
+    public static String convert(boolean x) {
+        if(x) {
+            return "'"+1+"'";
+        }
+        return "'"+0+"'";
+    }
+
+    public static double getDouble(String d) {
+        return Double.parseDouble(d.replaceAll("'", "").replaceAll(" ", ""));
+    }
+
+    public static int getInt(String i) {
+        return Integer.parseInt(i.replaceAll("'", "").replaceAll(" ", ""));
+    }
+
+    public static LocalDate getDate(String d) {
+        d = d.replaceAll("'", "").replaceAll(" ", "");
+        return LocalDate.of(getInt(d.substring(0, 4)), getInt(d.substring(5, 7)), getInt(d.substring(8, 10)));
+    }
+
+    public static boolean getBoolean(String b) {
+        if(getInt(b) == 0) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ ia/src/test/java/rrhs/xc/ia/data/database/SQLTypeConversionTest.java
@@ -0,0 +1,65 @@
+package rrhs.xc.ia.data.database;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+public class SQLTypeConversionTest {
+    
+    @Test
+    public void testInt() {
+        assertEquals("'123'", SQLTypeConversion.convert(123));
+        assertEquals("'-42'", SQLTypeConversion.convert(-42));
+
+        assertEquals(190, SQLTypeConversion.getInt("'190'"));
+        assertEquals(-67, SQLTypeConversion.getInt("'-67'"));
+        assertEquals(86, SQLTypeConversion.getInt("' 86    '"));
+        assertEquals(9, SQLTypeConversion.getInt("'09'"));
+
+        assertEquals(0, SQLTypeConversion.getInt("'0'"));
+    }
+
+    @Test
+    public void testDouble() {
+        assertEquals("'1.23'", SQLTypeConversion.convert(1.23));
+        assertEquals("'-4.2'", SQLTypeConversion.convert(-4.2));
+        assertEquals("'-0.2'", SQLTypeConversion.convert(-.2));
+
+        assertEquals(19.0, SQLTypeConversion.getDouble("'19.0'"));
+        assertEquals(-0.67, SQLTypeConversion.getDouble("'-.67'"));
+        assertEquals(86.0, SQLTypeConversion.getDouble("' 86    '"));
+        assertEquals(8.6, SQLTypeConversion.getDouble("' 8.6    '"));
+        assertEquals(9.0, SQLTypeConversion.getDouble("'09'"));
+        assertEquals(9.5, SQLTypeConversion.getDouble("'09.5'"));
+
+        assertEquals(0.0, SQLTypeConversion.getDouble("'0'"));
+    }
+
+    @Test
+    public void testBoolean() {
+        assertEquals("'0'", SQLTypeConversion.convert(false));
+        assertEquals("'1'", SQLTypeConversion.convert(true));
+
+        assertTrue(SQLTypeConversion.getBoolean("'1'"));
+        assertFalse(SQLTypeConversion.getBoolean("'0'"));
+    }
+
+    @Test
+    public void testString() {
+        assertEquals("'Jake Herrmann'", SQLTypeConversion.convert("Jake Herrmann"));
+    }
+
+    @Test
+    public void testLocalDate() {
+        assertEquals("'2024-01-01'", SQLTypeConversion.convert(LocalDate.of(2024, 1, 1)));
+        assertEquals("'2023-12-31'", SQLTypeConversion.convert(LocalDate.of(2023, 12, 31)));
+
+        assertEquals(LocalDate.of(1970, 1, 1), SQLTypeConversion.getDate("'1970-01-01'"));
+        assertEquals(LocalDate.of(1894, 11, 19), SQLTypeConversion.getDate("'1894-11-19'"));
+    }
+
+}
