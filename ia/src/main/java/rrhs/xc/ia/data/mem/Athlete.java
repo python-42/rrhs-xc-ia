590b640b75f7d665970d355e096432915f2b4402
Jake Herrmann <herrmann.jake@protonmail.com>
2024-01-29 12:55:44 -0600

    Added core functionality of Athlete class with test

--- ia/src/main/java/rrhs/xc/ia/data/mem/Athlete.java
+++ ia/src/main/java/rrhs/xc/ia/data/mem/Athlete.java
@@ -1,24 +1,100 @@
 package rrhs.xc.ia.data.mem;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
+import rrhs.xc.ia.data.Season;
 import rrhs.xc.ia.data.database.SQLRow;
 import rrhs.xc.ia.data.database.SQLTypeConversion;
 import rrhs.xc.ia.data.database.SQLTypeConversion.SQLTableInformation;
 import rrhs.xc.ia.data.i.PDFExportable;
 import rrhs.xc.ia.data.i.SQLSerializable;
 
-public class Athlete implements PDFExportable, SQLSerializable{
+public class Athlete implements PDFExportable, SQLSerializable {
 
     private String name;
     private boolean isBoysTeam;
     private int gradYear;
-    private Race[] races;
+    private HashMap<Season, List<Race>> raceMap = new HashMap<Season, List<Race>>();
 
     private boolean canSqlWrite = true;
 
-    public Athlete(Race[] races) {
-        this.races = races;
+    public Athlete(List<Race> list) {
+        if (list == null) {
+            return;
+        }
+        for (Race r : list) {
+            if (raceMap.get(r.getSeason()) == null) {
+                raceMap.put(r.getSeason(), new ArrayList<Race>());
+            }
+            raceMap.get(r.getSeason()).add(r);
+        }
+    }
+
+    /**
+     * Get the race with the best time. If the list contains races with identical
+     * times, one of them will be returned. There is no guarantee of which one.
+     * 
+     * If the list is null or empty, return null.
+     * 
+     * @param list a list of race objects.
+     * @return the fastest race or null
+     */
+    private Race getFastest(List<Race> list) {
+        if(list == null || list.size() == 0) {
+            return null;
+        }
+
+        int index = 0;
+        for (int i = 1; i < list.size(); i ++) {
+            if(list.get(i).getTimeSeconds() < list.get(index).getTimeSeconds()) {
+                index = i;
+            }
+        }
+        
+        return list.get(index);
+    }
+
+    /**
+     * Get the race with the worst time. If the list contains race with identical
+     * times, one of them will be returned. There is no guarnatee of which one.
+     * 
+     * If the list is null or empty, return null.
+     * 
+     * @param list a list of race objects.
+     * @return the slowest race or null
+     */
+    private Race getSlowest(List<Race> list) {
+        if(list == null || list.size() == 0) {
+            return null;
+        }
+        int index = 0;
+        for (int i = 1; i < list.size(); i ++) {
+            if(list.get(i).getTimeSeconds() > list.get(index).getTimeSeconds()) {
+                index = i;
+            }
+        }
+        
+        return list.get(index);
+    }
+
+    /**
+     * Condense all of the races from the HashMap in to a single list.
+     * 
+     * @return All the races from the map in a single list
+     */
+    private List<Race> condense() {
+        List<Race> races = new ArrayList<Race>();
+        for (List<Race> r : raceMap.values()) {
+            if (r == null) {
+                continue;
+            }
+            races.addAll(r);
+        }
+        return races;
     }
 
     public String getName() {
@@ -31,6 +107,122 @@ public class Athlete implements PDFExportable, SQLSerializable{
 
     public int getGradYear() {
         return gradYear;
+    }
+
+    public Race getWorstCareerRace() {
+        return getSlowest(condense());
+    }
+
+    public Race getWorstSeasonRace(Season s) {
+        return getSlowest(raceMap.get(s));
+
+    }
+
+    public Race getBestCareerRace() {
+        return getFastest(condense());
+    }
+
+    public Race getBestSeasonRace(Season s) {
+        return getFastest(raceMap.get(s));
+    }
+
+    public double getCareerTimeDropSeconds() {
+        Race best = getBestCareerRace();
+        Race worst = getWorstCareerRace();
+
+        if(best != null && worst != null) {
+            return worst.getTimeSeconds() - best.getTimeSeconds();
+        }
+        return -1;
+    }
+
+    public double getSeasonTimeDropSeconds(Season s) {
+        Race best = getBestSeasonRace(s);
+        Race worst = getWorstSeasonRace(s);
+
+        if(best != null && worst != null) {
+            return worst.getTimeSeconds() - best.getTimeSeconds();
+        }
+        return -1;
+    }
+
+    public double getCareerAverageTime() {
+        double totalTime = 0;
+        int count = 0;
+        for (Race r : condense()) {
+            totalTime += r.getTimeSeconds();
+            count++;
+        }
+
+        if(count == 0) {
+            return -1;
+        }
+
+        return totalTime / count;
+    }
+
+    public double getSeasonAverageTime(Season s) {
+        if(raceMap.get(s) == null || raceMap.get(s).size() == 0) {
+            return -1;
+        }
+
+        double totalTime = 0;
+        for (Race r : raceMap.get(s)) {
+            totalTime += r.getTimeSeconds();
+        }
+
+        return totalTime / raceMap.size();
+    }
+
+    public Season[] getSeasons() {
+        Season[] s = {};
+        s = raceMap.keySet().toArray(s);
+        Arrays.sort(s);
+        return s;
+    }
+
+    public double getAverageCareerSplitSeconds(int mile) {
+        if (mile > 3 || mile < 1) {
+            return -1;
+        }
+
+        double totalTime = 0;
+        int count = 0;
+        for (Race r : condense()) {
+            count++;
+            if(mile == 1) {
+                totalTime += r.getMileOneSplitSeconds();
+            }else if(mile == 2) {
+                totalTime += r.getMileTwoSplitSeconds();
+            }else {
+                totalTime += r.getMileThreeSplitSeconds();
+            }
+        }
+
+        if(count == 0) {
+            return -1;
+        }
+
+        return totalTime / count;
+    }
+
+    public double getAverageSeasonSplitSeconds(Season s, int mile) {
+        if (mile > 4 || mile < 1 || raceMap.get(s) == null || raceMap.get(s).size() == 0) {
+            return -1;
+        }
+
+        double totalTime = 0;
+        for (Race r : raceMap.get(s)) {
+            if(mile == 1) {
+                totalTime += r.getMileOneSplitSeconds();
+            }else if(mile == 2) {
+                totalTime += r.getMileTwoSplitSeconds();
+            }else {
+                totalTime += r.getMileThreeSplitSeconds();
+            }
+        }
+
+        return totalTime / raceMap.get(s).size();
     }
 
     @Override
@@ -60,5 +252,5 @@ public class Athlete implements PDFExportable, SQLSerializable{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'writeToPDF'");
     }
-    
+
 }
---
+++ ia/src/test/java/rrhs/xc/ia/data/mem/AthleteTest.java
@@ -0,0 +1,164 @@
+package rrhs.xc.ia.data.mem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import rrhs.xc.ia.data.Season;
+import rrhs.xc.ia.data.TestSQLRowUtil;
+import rrhs.xc.ia.data.database.SQLRow;
+
+public class AthleteTest {
+
+    private static Athlete a1;
+    private static Athlete empty;
+
+    private static Race r1;
+    private static Race r2;
+    private static Race r3;
+
+    @BeforeAll
+    public static void setup() {
+        SQLRow row1 = TestSQLRowUtil.race1;
+        SQLRow row2 = TestSQLRowUtil.race2;
+        SQLRow row3 = TestSQLRowUtil.race3;
+
+        TestSQLRowUtil.populateRaceRows();
+
+        r1 = new Race(); //SENIOR, fast
+        r2 = new Race(); //FRESHMAN
+        r3 = new Race(); //SENIOR, slower
+
+        r1.loadFromSQL(row1);
+        r2.loadFromSQL(row2);
+        r3.loadFromSQL(row3);
+
+        a1 = new Athlete(List.of(r1, r2, r3));
+        empty = new Athlete(List.of());
+    }
+
+    @Test
+    public void testGetWorstSeasonRace() {
+        assertEquals(r3, a1.getWorstSeasonRace(Season.SENIOR));
+        assertEquals(r2, a1.getWorstSeasonRace(Season.FRESHMAN));
+
+        assertEquals(null, empty.getWorstSeasonRace(Season.FRESHMAN));
+        assertEquals(null, empty.getWorstSeasonRace(Season.SOPHOMORE));
+        assertEquals(null, empty.getWorstSeasonRace(Season.JUNIOR));
+        assertEquals(null, empty.getWorstSeasonRace(Season.SENIOR));
+    }
+
+    @Test
+    public void testGetBestSeasonRace() {
+        assertEquals(r1, a1.getBestSeasonRace(Season.SENIOR));
+        assertEquals(r2, a1.getBestSeasonRace(Season.FRESHMAN));
+
+        assertEquals(null, empty.getBestSeasonRace(Season.FRESHMAN));
+        assertEquals(null, empty.getBestSeasonRace(Season.SOPHOMORE));
+        assertEquals(null, empty.getBestSeasonRace(Season.JUNIOR));
+        assertEquals(null, empty.getBestSeasonRace(Season.SENIOR));
+    }
+
+    @Test
+    public void testGetWorstCareerRace() {
+        assertEquals(r3, a1.getWorstCareerRace());
+
+        assertEquals(null, empty.getWorstCareerRace());
+    }
+
+    @Test
+    public void testGetBestCareerRace() {
+        assertEquals(r1, a1.getBestCareerRace());
+
+        assertEquals(null, empty.getBestCareerRace());
+    }
+
+    @Test
+    public void testGetCareerTimeDrop() {
+        assertEquals((9 * 60), a1.getCareerTimeDropSeconds());  
+
+        assertEquals(-1, empty.getCareerTimeDropSeconds());
+    }
+
+    @Test
+    public void testSeasonTimeDrop() {
+        assertEquals((9 * 60), a1.getSeasonTimeDropSeconds(Season.SENIOR));
+
+        assertEquals(0, a1.getSeasonTimeDropSeconds(Season.FRESHMAN));
+
+        assertEquals(-1, empty.getSeasonTimeDropSeconds(Season.FRESHMAN));
+        assertEquals(-1, empty.getSeasonTimeDropSeconds(Season.SOPHOMORE));
+        assertEquals(-1, empty.getSeasonTimeDropSeconds(Season.JUNIOR));
+        assertEquals(-1, empty.getSeasonTimeDropSeconds(Season.SENIOR));
+    }
+
+    @Test
+    public void testCareerAverageTime() {
+        assertEquals(1329.2, a1.getCareerAverageTime());
+
+        assertEquals(-1, empty.getCareerAverageTime());
+    }
+
+    
+    @Test
+    public void testSeasonAverageTime() {
+        assertEquals(1338.5, a1.getSeasonAverageTime(Season.SENIOR));
+
+        assertEquals(-1, empty.getSeasonAverageTime(Season.FRESHMAN));
+        assertEquals(-1, empty.getSeasonAverageTime(Season.SOPHOMORE));
+        assertEquals(-1, empty.getSeasonAverageTime(Season.JUNIOR));
+        assertEquals(-1, empty.getSeasonAverageTime(Season.SENIOR));
+    }
+
+ 
+    @Test
+    public void testGetSeasons() {
+        assertEquals(2, a1.getSeasons().length);
+        assertEquals(Season.FRESHMAN, a1.getSeasons()[0]);
+        assertEquals(Season.SENIOR, a1.getSeasons()[1]);
+
+        assertEquals(0, empty.getSeasons().length);
+    }
+
+    
+    @Test
+    public void testGetAverageCareerSplit() {
+        assertEquals(415, a1.getAverageCareerSplitSeconds(1));
+
+        assertEquals(-1, empty.getAverageCareerSplitSeconds(1));
+        assertEquals(-1, empty.getAverageCareerSplitSeconds(2));
+        assertEquals(-1, empty.getAverageCareerSplitSeconds(3));
+        assertEquals(-1, empty.getAverageCareerSplitSeconds(4));
+    }
+
+    
+    @Test
+    public void testGetAverageSeasonSplit() {
+        assertEquals(413, a1.getAverageSeasonSplitSeconds(Season.SENIOR, 1));
+        assertEquals(-1, a1.getAverageSeasonSplitSeconds(Season.JUNIOR, 1));
+
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.FRESHMAN, 1));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.SOPHOMORE, 1));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.JUNIOR, 1));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.SENIOR, 1));
+
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.FRESHMAN, 2));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.SOPHOMORE, 2));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.JUNIOR, 2));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.SENIOR, 2));
+
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.FRESHMAN, 3));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.SOPHOMORE, 3));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.JUNIOR, 3));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.SENIOR, 3));
+
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.FRESHMAN, 4));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.SOPHOMORE, 4));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.JUNIOR, 4));
+        assertEquals(-1, empty.getAverageSeasonSplitSeconds(Season.SENIOR, 4));
+    }
+
+}
