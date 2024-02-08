c9b41b29c349f64a0918b14ed8919876fd532068
Jake Herrmann <herrmann.jake@protonmail.com>
2024-02-08 13:08:37 -0600

    Switched main controller location

--- ia/src/main/java/rrhs/xc/ia/ui/controller/MainController.java
+++ ia/src/main/java/rrhs/xc/ia/ui/controller/MainController.java
@@ -1,19 +1,15 @@
-package rrhs.xc.ia.ui;
+package rrhs.xc.ia.ui.controller;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import rrhs.xc.ia.data.mem.Athlete;
 import rrhs.xc.ia.data.mem.Meet;
-import rrhs.xc.ia.ui.controller.ISceneController;
 import rrhs.xc.ia.ui.event.SceneEvent;
 import rrhs.xc.ia.util.SearchChangeListener;
 
@@ -52,35 +48,29 @@ public class MainController implements ISceneController{
 
         athleteSearch.textProperty().addListener(new SearchChangeListener<Athlete>(viewableAthletes, allAthletes));
 
-        editRosterBtn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent arg0) {
-                editRosterBtn.fireEvent(new SceneEvent("roster"));
-            }
-            
-        });
-    }
-    
-    public void setAthletes(List<Athlete> list) {
-        allAthletes.clear();
-        allAthletes.addAll(list);
-        viewableAthletes.clear();
-        viewableAthletes.addAll(list);
-        athleteSearch.setText("");
-    }
-
-    public void setMeets(List<Meet> list) {
-        allMeets.clear();
-        allMeets.addAll(list);
-        viewableMeets.clear();
-        viewableMeets.addAll(list);
-        meetSearch.setText("");
+        editRosterBtn.setOnAction(event -> editRosterBtn.fireEvent(new SceneEvent("roster")));
     }
 
     @Override
-    public Scene getScene() {
-        return meetSearch.getScene();
+    public void setupAthletes(List<Athlete> list) {
+        if (list != null) {
+            allAthletes.clear();
+            viewableAthletes.clear();
+
+            allAthletes.addAll(list);
+            viewableAthletes.addAll(list);
+        } 
+    }
+
+    @Override
+    public void setupMeets(List<Meet> list) {
+        if (list != null) {
+            allMeets.clear();
+            viewableMeets.clear();
+
+            allMeets.addAll(list);
+            viewableMeets.addAll(list);
+        }
     }
 
 }
