1d232f86cb9bfa19fc2c5ce23cfad571f7c9b0ac
Jake Herrmann <herrmann.jake@protonmail.com>
2024-02-20 11:15:33 -0600

    Changed athlete stat view to match meet stat view

--- ia/src/main/java/rrhs/xc/ia/ui/controller/AthleteController.java
+++ ia/src/main/java/rrhs/xc/ia/ui/controller/AthleteController.java
@@ -1,6 +1,8 @@
 package rrhs.xc.ia.ui.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.AbstractMap.SimpleEntry;
 
 import org.jfree.chart.ChartPanel;
 
@@ -9,22 +11,13 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.TilePane;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Paint;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import rrhs.xc.ia.data.i.PDFExportable;
 import rrhs.xc.ia.data.mem.Athlete;
@@ -43,6 +36,7 @@ public class AthleteController implements SceneController {
     @FXML private Tab graphTab;
 
     @FXML private TableView<Race> table;
+
     @FXML private TableColumn<Race, String> meetName;
     @FXML private TableColumn<Race, String> meetDate;
     @FXML private TableColumn<Race, String> time;
@@ -53,7 +47,18 @@ public class AthleteController implements SceneController {
     @FXML private TableColumn<Race, Integer> place;
     @FXML private TableColumn<Race, String> varsity;
 
-    @FXML private TilePane statsPane;
+
+    @FXML private VBox freshmanStats;
+    @FXML private VBox sophomoreStats;
+    @FXML private VBox juniorStats;
+    @FXML private VBox seniorStats;
+
+    @FXML private VBox careerStats;
+
+    @FXML private TitledPane freshmanStatsContainer;
+    @FXML private TitledPane sophomoreStatsContainer;
+    @FXML private TitledPane juniorStatsContainer;
+    @FXML private TitledPane seniorStatsContainer;
 
     private Athlete athlete;
 
@@ -74,11 +79,6 @@ public class AthleteController implements SceneController {
             (cellData.getValue().getLevel() == Level.VARSITY_BOYS || cellData.getValue().getLevel() == Level.VARSITY_GIRLS)
             ? "Yes" : "No"
         ));
-
-        statsPane.setPrefColumns(5);
-        statsPane.setHgap(15);
-        statsPane.setAlignment(Pos.TOP_CENTER);
-        statsPane.setPadding(new Insets(50));
     }
 
     @Override
@@ -97,57 +97,41 @@ public class AthleteController implements SceneController {
     }
 
     private void setupStats() {
-        statsPane.getChildren().clear();
-        Border b = new Border(new BorderStroke(Paint.valueOf("black"), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.MEDIUM));
+        HashMap<Season, SimpleEntry<TitledPane, VBox>> statMap = new HashMap<Season, SimpleEntry<TitledPane, VBox>>();
+        statMap.put(Season.FRESHMAN, new SimpleEntry<TitledPane,VBox>(freshmanStatsContainer, freshmanStats));
+        statMap.put(Season.SOPHOMORE, new SimpleEntry<TitledPane,VBox>(sophomoreStatsContainer, sophomoreStats));
+        statMap.put(Season.JUNIOR, new SimpleEntry<TitledPane,VBox>(juniorStatsContainer, juniorStats));
+        statMap.put(Season.SENIOR, new SimpleEntry<TitledPane,VBox>(seniorStatsContainer, seniorStats));
 
         for (Season season : athlete.getSeasons()) {
-            VBox box = new VBox();
-            box.setBorder(b);
-            box.setPadding(new Insets(10));
-            box.setAlignment(Pos.CENTER);
-            ObservableList<Node> children = box.getChildren();
+            ObservableList<Node> children = statMap.get(season).getValue().getChildren();
 
-            Text title = new Text(season.name());
-            title.setFont(new Font(15));
-            title.setUnderline(true);            
-            children.add(title);
-
-            children.add(new Label("Time Drop: " + StringUtils.formatTime(athlete.getSeasonTimeDropSeconds(season))));
-            children.add(new Label("Average Time: " + StringUtils.formatTime(athlete.getSeasonAverageTime(season))));
-            children.add(new Label("Average Mile 1 Split: " + StringUtils.formatTime(athlete.getAverageSeasonSplitSeconds(season, 1))));
-            children.add(new Label("Average Mile 2 Split: " + StringUtils.formatTime(athlete.getAverageSeasonSplitSeconds(season, 2))));
-            children.add(new Label("Average Mile 3 Split: " + StringUtils.formatTime(athlete.getAverageSeasonSplitSeconds(season, 3))));
+            children.add(new Text("Time Drop: " + StringUtils.formatTime(athlete.getSeasonTimeDropSeconds(season))));
+            children.add(new Text("Average Time: " + StringUtils.formatTime(athlete.getSeasonAverageTime(season))));
+            children.add(new Text("Average Mile 1 Split: " + StringUtils.formatTime(athlete.getAverageSeasonSplitSeconds(season, 1))));
+            children.add(new Text("Average Mile 2 Split: " + StringUtils.formatTime(athlete.getAverageSeasonSplitSeconds(season, 2))));
+            children.add(new Text("Average Mile 3 Split: " + StringUtils.formatTime(athlete.getAverageSeasonSplitSeconds(season, 3))));
 
             Race best = athlete.getBestSeasonRace(season);
             if (best != null) {
-                children.add(new Label("Best Race: " + best.getMeetName().trim() + " (" + StringUtils.formatTime(best.getTimeSeconds()) + ")"));
+                children.add(new Text("Best Race: " + best.getMeetName().trim() + " (" + StringUtils.formatTime(best.getTimeSeconds()) + ")"));
             }
 
-            statsPane.getChildren().add(box);
+            statMap.get(season).getKey().setExpanded(true);
+
         }
-        VBox box = new VBox();
-        box.setBorder(b);
-        box.setPadding(new Insets(10));
-        box.setAlignment(Pos.CENTER);
-        ObservableList<Node> children = box.getChildren();
+        ObservableList<Node> children = careerStats.getChildren();
 
-        Text title = new Text("CAREER");
-        title.setFont(new Font(15));
-        title.setUnderline(true);
-        children.add(title);
-
-        children.add(new Label("Time Drop: " + StringUtils.formatTime(athlete.getCareerTimeDropSeconds())));
-        children.add(new Label("Average Time: " + StringUtils.formatTime(athlete.getCareerAverageTime())));
-        children.add(new Label("Average Mile 1 Split: " + StringUtils.formatTime(athlete.getAverageCareerSplitSeconds(1))));
-        children.add(new Label("Average Mile 2 Split: " + StringUtils.formatTime(athlete.getAverageCareerSplitSeconds(2))));
-        children.add(new Label("Average Mile 3 Split: " + StringUtils.formatTime(athlete.getAverageCareerSplitSeconds(3))));
+        children.add(new Text("Time Drop: " + StringUtils.formatTime(athlete.getCareerTimeDropSeconds())));
+        children.add(new Text("Average Time: " + StringUtils.formatTime(athlete.getCareerAverageTime())));
+        children.add(new Text("Average Mile 1 Split: " + StringUtils.formatTime(athlete.getAverageCareerSplitSeconds(1))));
+        children.add(new Text("Average Mile 2 Split: " + StringUtils.formatTime(athlete.getAverageCareerSplitSeconds(2))));
+        children.add(new Text("Average Mile 3 Split: " + StringUtils.formatTime(athlete.getAverageCareerSplitSeconds(3))));
 
         Race best = athlete.getBestCareerRace();
         if (best != null) {
-            children.add(new Label("Best Race: " + best.getMeetName().trim() + " (" + StringUtils.formatTime(best.getTimeSeconds()) + ")"));
+            children.add(new Text("Best Race: " + best.getMeetName().trim() + " (" + StringUtils.formatTime(best.getTimeSeconds()) + ")"));
         }
-
-        statsPane.getChildren().add(box);
 
     }
 
