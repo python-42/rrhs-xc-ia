5cb328fc33bbb2cc6c62a9fd7f764e84d5c0422f
Jake Herrmann <herrmann.jake@protonmail.com>
2024-02-27 21:09:39 -0600

    Deleted now-unused files and methods

--- ia/src/main/java/rrhs/xc/ia/util/StringUtils.java
+++ ia/src/main/java/rrhs/xc/ia/util/StringUtils.java
@@ -1,6 +1,5 @@
 package rrhs.xc.ia.util;
 
-import java.io.File;
 import java.util.regex.Pattern;
 
 public class StringUtils {
@@ -47,10 +46,5 @@ public class StringUtils {
     public static boolean matchesRegex(String regex, String test) {
         Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         return p.matcher(test).find();
-    }
-
-    public static String removeFilExtension(File f) {
-        String rtn = f.getName();
-        return rtn.substring(0, rtn.lastIndexOf('.'));
     }
 }
--- ia/src/test/java/rrhs/xc/ia/data/integration/StringUtilsTest.java
+++ ia/src/test/java/rrhs/xc/ia/data/integration/StringUtilsTest.java
@@ -2,22 +2,11 @@ package rrhs.xc.ia.data.integration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.File;
-
 import org.junit.jupiter.api.Test;
 
 import rrhs.xc.ia.util.StringUtils;
 
 public class StringUtilsTest {
-
-    @Test
-    public void testRemoveFileExt() {
-        assertEquals("file", StringUtils.removeFilExtension(new File("file.fxml")));
-
-        assertEquals("cool file!", StringUtils.removeFilExtension(new File("cool file!.txt")));
-
-        assertEquals("i.use.dots", StringUtils.removeFilExtension(new File("i.use.dots.png")));
-    }
 
     @Test
     public void testFormatTime() {
